7416e7a0219a3e51ce9bb4c89ae9da00eacaf431
Oscar Mercado <omercador@unicartagena.edu.co>
2024-11-20 08:21:41 -0500

    Correcion de ejercicio 3

---
+++ Protocolo Individual und 3/Taller11/src/Ejercicio3/Animal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio3;
+
+/**
+ *
+ * @author Lenovo
+ */
+abstract class Animal {
+    
+    // Método abstracto (sin implementación)
+    public abstract void hacerSonido();
+
+    // Método concreto (con implementación)
+    public void dormir() {
+        System.out.println("El animal está durmiendo.");
+    }
+    
+}
